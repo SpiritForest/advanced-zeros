f55018cec20d399b2e4682cda5142ef9c45994e5
Dzmitry Zakhar <31858665+SpiritForest@users.noreply.github.com>
2018-03-11 19:41:11 +0300

    2
    2

--- src/index.js
+++ src/index.js
@@ -1,20 +1,43 @@
 module.exports = function getZerosCount(number, base) {
-  var counter = 0, b,  n;
-    for (var i = base - 1; i > 1; i--){
-        if (base%i == 0) {
-            n = i;
-            break;
-        }
-        if (base%i != 0) {
-            n = base;
-        }
+let simple = [], exp = [], zeros = [];
+let counter = 0, b = base, summ = 0, num = number, mult, ex = 2;
 
+//finding all simple number
+    for (let i = 2; i <= b; i++){
+    if (b%i == 0) {
+    simple.push(i);
+    }
+        while (b%i == 0){
+            b /= i;
+            counter++;
+            }
+
+        if (counter > 0) {
+            exp.push(counter);
+            }
+        counter = 0;
     }
 
-    b = n;
-    while (number >= n){
-        counter += Math.floor(number/n);
-        n = n*b;
+    //creating mass with numbers
+    for (var j = 0; j < simple.length; j++){
+            mult = simple[j];
+
+        while (num/mult > 1){
+            summ += Math.floor(num/mult);
+            mult = Math.pow(simple[j], ex);
+            ex++;
+        }
+        zeros.push(summ);
+        summ = 0;
+        ex = 2;
     }
-  return  counter;
+
+    //finding the min length of zeros
+    for (let k = 0; k < zeros.length; k++){
+        zeros[k] = Math.floor(zeros[k]/exp[k]);
+    }
+
+    zeros.sort(function(a, b){return a-b;});
+    return zeros[0];
+
 }
